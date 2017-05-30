6a3749b9771493694cad6c0a6a3efc3a758d2e4c
Miguel Angel <hdl4fpga@users.noreply.github.com>
2017-05-30 21:12:37 +0200

    Delete upper.sh

--- upper.sh
+++
@@ -1,4 +0,0 @@
-for i in sclk_phases sclk_edges data_phases data_edges cmd_phases bank_size addr_size word_size line_size byte_size data_gear ; do
-	e=`echo $i | tr '[:lower:]' '[:upper:]'` 
-	sed -i.bak s/$i/$e/g $1
-done
