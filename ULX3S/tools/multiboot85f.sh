041296830c8e224c1316f72e7d33476a6760be1a
emard <vordah@gmail.com>
2019-07-03 18:18:16 +0200

    ULX3S multiboot scripts update

--- ULX3S/tools/multiboot85f.sh
+++ ULX3S/tools/multiboot85f.sh
@@ -2,8 +2,8 @@
 LANG=C LD_LIBRARY_PATH=/mt/scratch/tmp/openfpga/prjtrellis/libtrellis /mt/scratch/tmp/openfpga/prjtrellis/libtrellis/ecpmulti \
   --db /mt/scratch/tmp/openfpga/prjtrellis/database \
   --flashsize 128 \
-  --input-idcode 0x41111043 \
-  --input /tmp/ulx3s_12f_scope_oled.bit \
-  --input /tmp/ulx3s_12f_scope_dvi.bit --address 0x100000 \
-  --output-idcode 0x21111043 \
-  --output /tmp/ulx3s_12f_scope_oled_dvi.bit
+  --input-idcode 0x41113043 \
+  --input /tmp/ulx3s_85f_scope_oled.bit \
+  --input /tmp/ulx3s_85f_scope_dvi.bit --address 0x200000 \
+  --output-idcode 0x41113043 \
+  --output /tmp/ulx3s_85f_scope_oled_dvi.bit
