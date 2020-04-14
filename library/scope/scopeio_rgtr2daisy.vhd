1e7e333e31763d0dff574adebc5deae4b706568d
emard <vordah@gmail.com>
2020-04-14 20:10:38 +0200

    scopeio_rgtr2daisy.vhd: bit reverse workaround to get 2-bit mode working

--- library/scope/scopeio_rgtr2daisy.vhd
+++ library/scope/scopeio_rgtr2daisy.vhd
@@ -37,6 +37,7 @@ architecture beh of scopeio_rgtr2daisy is
 	constant C_pointer_packet_length : integer := 5*rgtr_id'length; -- 5 bytes
 	signal R_shift     : std_logic_vector(C_pointer_packet_length + C_rgtr_packet_length - 1 downto 0);
 	signal R_pad0      : std_logic_vector(chaino_data'range) := (others => '0');
+	signal S_reverse_packet, S_normal_packet: std_logic_vector(R_shift'range);
 	constant C_pointer_and_rgtr_cycles  : integer := R_shift'length / chaino_data'length;
 	constant C_pointer_only_cycles: integer := C_pointer_packet_length / chaino_data'length;
 	signal R_counter   : unsigned(unsigned_num_bits(C_pointer_and_rgtr_cycles)-1 downto 0);
@@ -44,26 +45,49 @@ begin
 	assert chaino_data'length=chaini_data'length 
 		report "chaino_data'length not equal chaini_data'length"
 		severity failure;
+	S_normal_packet <= x"15" & x"02" & "00" & pointer_y & pointer_x
+		      & rgtr_id & x"03" & rgtr_data;
+	G_bytes_reversal:
+	for i in 0 to S_reverse_packet'length/8-1 generate
+		S_reverse_packet(i*8+7 downto i*8) <= -- reverse(S_normal_packet(i*2+1 downto i*2));
+		  --  reverse(S_normal_packet(i*8+7 downto i*8));
+		    S_normal_packet(i*8+1)
+		  & S_normal_packet(i*8+0)
+		  & S_normal_packet(i*8+3)
+		  & S_normal_packet(i*8+2)
+		  & S_normal_packet(i*8+5)
+		  & S_normal_packet(i*8+4)
+		  & S_normal_packet(i*8+7)
+		  & S_normal_packet(i*8+6);
+		  --  S_normal_packet(i*8+6)
+		  --& S_normal_packet(i*8+7)
+		  --& S_normal_packet(i*8+4)
+		  --& S_normal_packet(i*8+5)
+		  --& S_normal_packet(i*8+2)
+		  --& S_normal_packet(i*8+3)
+		  --& S_normal_packet(i*8+0)
+		  --& S_normal_packet(i*8+1);
+	end generate;
 	process(clk)
 	begin
 		if rising_edge(clk) then
 			-- if R_strm_frm is 1 the request will be dropped
 			-- to prevent overwriting currently active serializing
-			if (rgtr_dv = '1' or pointer_dv = '1') and R_strm_frm = '0' and (chaini_frm = '0' or chaini_irdy = '0') then
-				R_shift <= x"15" & x"02" & "00" & pointer_y & pointer_x
-				         & rgtr_id & x"03" & rgtr_data;
-				if rgtr_dv = '1' and rgtr_id /= x"00" then
-					R_counter <= to_unsigned(C_pointer_and_rgtr_cycles - 1, R_counter'length);
-				else
-					R_counter <= to_unsigned(C_pointer_only_cycles - 1, R_counter'length);
+			if R_counter = 0 then
+				R_strm_frm <= '0';
+				if (rgtr_dv = '1' or pointer_dv = '1') then
+					if R_strm_frm = '0' and (chaini_frm = '0' and chaini_irdy = '0') then
+						R_shift <= S_reverse_packet;
+						if rgtr_dv = '1' and rgtr_id /= x"00" then
+							R_counter <= to_unsigned(C_pointer_and_rgtr_cycles - 1, R_counter'length);
+						else
+							R_counter <= to_unsigned(C_pointer_only_cycles - 1, R_counter'length);
+						end if;
+						R_strm_frm <= '1';
+					end if;
 				end if;
-				R_strm_frm <= '1';
 			else
-				if R_counter = 0 then
-					R_strm_frm <= '0';
-				else
-					R_counter <= R_counter - 1;
-				end if;
+				R_counter <= R_counter - 1;
 				R_shift <= R_shift(R_shift'high - R_pad0'length downto 0) & R_pad0;
 			end if;
 		end if;
