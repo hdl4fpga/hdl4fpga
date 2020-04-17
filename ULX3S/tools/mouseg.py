d05c9a7d13b1151a15f8d506b596d74481c8e42d
emard <vordah@gmail.com>
2020-04-17 13:38:43 +0200

    mouseg.py: select grid color with keys 0-4 and correct wheel direction

--- ULX3S/tools/mouseg.py
+++ ULX3S/tools/mouseg.py
@@ -12,7 +12,7 @@ import socket
 
 serial_port = "/dev/ttyUSB0"
 udp_host = "192.168.18.166"
-# udp_port = 57001  # use UDP
+#udp_port = 57001  # use UDP
 udp_port = False  # use SERIAL
 
 if udp_port:
@@ -33,16 +33,27 @@ def escape(p):
   return retval
   
 def pointer(x,y):
-  rgtr = (y & 0xFFF)*(2**12) + (x & 0xFFF)
+  rgtr = ((y & 0xFFF)<<12) + (x & 0xFFF)
   return struct.pack(">BBI", 0x15, 3, rgtr)
 
 def mouse_report(dx,dy,dz,btn_left,btn_middle,btn_right):
-  return struct.pack(">BBBBBB", 0x0F, 3, (-dz) & 0xFF, (-dy) & 0xFF, dx & 0xFF, (btn_left & 1) + (btn_right & 1)*(2**1) + (btn_middle & 1)*(2**2))
+  return struct.pack(">BBBBBB", 0x0F, 3, (-dz) & 0xFF, (-dy) & 0xFF, dx & 0xFF, (btn_left & 1) + ((btn_right & 1)<<1) + ((btn_middle & 1)<<2))
 
 def print_packet(x):
   for c in x:
     print("%02X" % (c), end='');
   print("")
+
+def packet_grid_color(pid,c):
+  return struct.pack(">BBI", 0x11, 3, ((c & 63) << 9) | (pid << 3) | 0x7)
+
+def send_grid_color(c):
+  packet = packet_grid_color(0,c)
+  if udp_port:
+    scopeio_udp.sendto(packet, (udp_host, udp_port))
+  else:
+    scopeio.write(escape(packet))
+
 
 pygame.init()
 (width, height) = (320, 200)
@@ -63,6 +74,38 @@ while(True):
     if event.key == pygame.K_ESCAPE or event.key == pygame.K_PAUSE:
       print("QUIT")
       break
+    if event.key == pygame.K_0:
+      send_grid_color(0) # black
+    if event.key == pygame.K_1:
+      send_grid_color(48) # red
+    if event.key == pygame.K_2:
+      send_grid_color(12) # green
+    if event.key == pygame.K_3:
+      send_grid_color(39) # blue
+    if event.key == pygame.K_4:
+      send_grid_color(61) # yellow
+    if event.key == pygame.K_5:
+      send_grid_color(5)
+    if event.key == pygame.K_6:
+      send_grid_color(6)
+    if event.key == pygame.K_7:
+      send_grid_color(7)
+    if event.key == pygame.K_8:
+      send_grid_color(8)
+    if event.key == pygame.K_9:
+      send_grid_color(9)
+    if event.key == pygame.K_a:
+      send_grid_color(10)
+    if event.key == pygame.K_b:
+      send_grid_color(11)
+    if event.key == pygame.K_c:
+      send_grid_color(12)
+    if event.key == pygame.K_d:
+      send_grid_color(13)
+    if event.key == pygame.K_e:
+      send_grid_color(14)
+    if event.key == pygame.K_f:
+      send_grid_color(15)
     text = str(event.unicode)
     screen.blit(font.render(text, True, (255, 255, 255)), (0, 0))
     pygame.display.flip()
@@ -73,9 +116,9 @@ while(True):
   wheel = 0
   if event.type == pygame.MOUSEBUTTONDOWN: # for wheel events
     if event.button == 4: # wheel UP
-      wheel = -1
-    if event.button == 5: # wheel DOWN
       wheel = 1
+    if event.button == 5: # wheel DOWN
+      wheel = -1
   (dx, dy) = pygame.mouse.get_rel()
   dz = wheel
   (btn_left, btn_middle, btn_right) = pygame.mouse.get_pressed()
