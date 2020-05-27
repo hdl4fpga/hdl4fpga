52490f6b3d3a45c77749adea3df742e56a66976f
emard <vordah@gmail.com>
2020-05-27 14:51:28 +0200

    lvds out default, delete some unwanted "begin"

--- ULX3S/scopeio/scopeio_top.vhd
+++ ULX3S/scopeio/scopeio_top.vhd
@@ -29,7 +29,7 @@ architecture beh of ulx3s is
 	--10:  800x480  @ 60Hz  40MHz 16-pix grid 8-pix font 3 segments
 	--11:  480x272  @ 135Hz 25MHz 16-pix grid 8-pix font 1 segment
 	--12:  480x272  @ 135Hz 25MHz 16-pix grid 8-pix font 2 segments
-        constant vlayout_id: integer := 5;
+        constant vlayout_id: integer := 11;
         constant C_external_sync : std_logic := '0';
         -- GUI pointing device type (enable max 1)
         constant C_mouse_ps2    : boolean := false; -- PS/2 or USB+PS/2 mouse
@@ -83,8 +83,8 @@ architecture beh of ulx3s is
 	constant C_oled_hex_view_net : boolean := false;
 	constant C_oled_hex_view_istream: boolean := false;
 	-- DVI/LVDS/OLED VGA (enable only 1)
-        constant C_dvi_vga:  boolean := true;
-        constant C_lvds_vga: boolean := false;
+        constant C_dvi_vga:  boolean := false;
+        constant C_lvds_vga: boolean := true;
         constant C_oled_vga: boolean := false;
         constant C_oled_hex: boolean := false;
 
@@ -1669,7 +1669,6 @@ begin
     end generate;
 
     G_lvds_vga: if C_lvds_vga generate
-    begin
     E_vga2lvds: entity hdl4fpga.vga2lvds
     port map
     (
