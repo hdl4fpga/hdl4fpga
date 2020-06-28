4579859c42ed3d9048cbced4cf59616ce6c6b0b4
emard <vordah@gmail.com>
2020-06-28 10:40:41 +0200

    replace script updated

--- ULX3S/trellis/replace_std.sh
+++ ULX3S/trellis/replace_std.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # helper script to replace std->hdl4fpgapkg in all files
-find ../../library -name "*.vhd" -exec sed -i -e "s/hdl4fpga[.]std/hdl4fpga.hdl4fpgapkg/g" {} \;
-find ../scopeio    -name "*.vhd" -exec sed -i -e "s/hdl4fpga[.]std/hdl4fpga.hdl4fpgapkg/g" {} \;
+find ../../ -name "*.vhd" -exec sed -i -e "s/hdl4fpga[.]std/hdl4fpga.hdl4fpgapkg/g" {} \;
+# edit std.vhd file
+cp ../../library/common/std.vhd ../../library/common/hdl4fpgapkg.vhd
+sed -i -e "s/std is/hdl4fpgapkg is/g" ../../library/common/hdl4fpgapkg.vhd
