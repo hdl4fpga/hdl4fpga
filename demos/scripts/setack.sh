161a328416a66fb6ea64f4176bec79a9fbcdb0a2
Miguel Angel <hdl4fpga@gmail.com>
2022-01-07 13:37:28 -0300

    Delete setack.sh

--- demos/scripts/setack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-TTY="${TTY:-/dev/ttyUSB0}"
-SPEED="${SPEED:-3000000}"
-ACK="${1:-10}"
-
-ACK=`printf %02x ${ACK}`
-echo "00020000${ACK}"|xxd -r -ps|./scripts/siocomm.sh|xxd -ps| tr -d '\n'
