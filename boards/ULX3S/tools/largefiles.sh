b5de79c82920e2614be6d53f2aa0aad8e63eb8c8
Miguel Angel <hdl4fpga@gmail.com>
2023-04-02 12:52:18 -0300

    Delete boards/ULX3S/tools directory

--- boards/ULX3S/tools/largefiles.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git rev-list --objects --all | git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' | sed -n 's/^blob //p' | sort --numeric-sort --key=2 | cut -c 1-12,41- | $(command -v gnumfmt || echo numfmt) --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
--- boards/ULX3S/tools/multiboot12f.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-LANG=C LD_LIBRARY_PATH=/mt/scratch/tmp/openfpga/prjtrellis/libtrellis /mt/scratch/tmp/openfpga/prjtrellis/libtrellis/ecpmulti \
-  --db /mt/scratch/tmp/openfpga/prjtrellis/database \
-  --flashsize 128 \
-  --input-idcode 0x41111043 \
-  --input /tmp/ulx3s_12f_scope_oled.bit \
-  --input /tmp/ulx3s_12f_scope_dvi.bit --address 0x100000 \
-  --output-idcode 0x21111043 \
-  --output /tmp/ulx3s_12f_scope_oled_dvi.bit
