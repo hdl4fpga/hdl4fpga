4a19b6f34eda8f340184976abbd9fd9e0907a7e6
Miguel Angel <hdl4fpga@gmail.com>
2023-10-16 15:20:09 -0300

    Add files via upload

--- boards/ULX4M_LD/apps/graphics.vhd
+++ boards/ULX4M_LD/apps/graphics.vhd
@@ -42,7 +42,7 @@ architecture graphics of ulx4m_ld is
 
 	---------------------------------------
 	-- Set your profile here             --
-	constant io_link      : io_comms     := io_ipoe;
+	constant io_link      : io_comms     := io_usb;
 	constant sdram_speed  : sdram_speeds := sdram400MHz;
 	constant video_mode   : video_modes  := mode600p24bpp;
 	-- constant video_mode   : video_modes  := mode720p24bpp;
